3a674582fdaa10ee42bcd9a648dd2b7a66656e34
Alain Noel <alain.noel34@gmail.com>
2022-12-01 16:10:05 +0100

    add handleclick ok

--- src/components/MovieCard/index.jsx
+++ src/components/MovieCard/index.jsx
@@ -1,17 +1,13 @@
 import React, { useState } from "react";
 import styles from './movieCard.module.css';
-import { Modal, show } from 'react-bootstrap';
-import { Button } from 'react-bootstrap';
-import { useNavigate } from "react-router-dom";
-import { Navigate } from "react-router-dom";
+
 
 const API_IMG = "https://image.tmdb.org/t/p/w500";
 
-const MovieCard = ({movie, title, poster_path}) => {
-
+const MovieCard = ({movie, selectMovie, title, poster_path, backdrop_path}) => {
 
     return (
-      <div className={styles.movie__card}>
+      <div className={styles.movie__card} onClick={() => selectMovie(movie)}>
           {poster_path ? <img className={styles.card__img} style={{width:'14rem'}} src={API_IMG+poster_path} alt=""/>
           :
           <div className={styles.movie__placeholder}> No image found</div>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import MovieCard from "../../components/MovieCard";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from "./home.module.css";
+import SearchBar from "../../components/NavBar";
 
 
 const API_URL="https://api.themoviedb.org/3/movie/popular?api_key=eda25039ca2db0c207beac572dc11346";
@@ -14,7 +14,7 @@ const Home = () => {
 
   const [movies, setMovies] = useState([]);
   const [query, setQuery]=useState('');
-  const [selectMovie, setSelectMovie]= useState({});
+  const [selectMovie, setSelectedMovie]= useState([]);
 
   useEffect(() => {
      fetch(API_URL)
@@ -22,12 +22,13 @@ const Home = () => {
     .then(data=>{
       console.log(data);
       setMovies(data.results);
-      setSelectMovie(data.results[0]);
+      setSelectedMovie(data.results[0]);
+      console.log(data);
     })
-  }, [])
+    }, [])
 
-  const searchMovie = async(e)=>{
-    e.preventDefault();
+    const searchMovie = async(e)=>{
+      e.preventDefault();
     console.log("Searching");
     try{
       const url=`https://api.themoviedb.org/3/search/movie?api_key=eda25039ca2db0c207beac572dc11346&query=${query}`;
@@ -35,6 +36,7 @@ const Home = () => {
       const data= await res.json();
       console.log(data);
       setMovies(data.results);
+      setSelectedMovie(data.results[0]);
     }
     catch(e){
       console.log(e);
@@ -47,10 +49,11 @@ const Home = () => {
 
   return (
     <div className="general">
+      <SearchBar />
         <div className={styles.header}>
             <div className={styles.header__content}>
                 <h1>AlloMovies</h1>
-                <form className="d-flex" onSubmit={searchMovie} autoComplete="off">
+                <form className="d-flex" onSubmit={searchMovie} autoComplete="on">
                       <input type="search"
                       placeholder="Enter Movie Name"
                       className="inputText"
@@ -58,7 +61,7 @@ const Home = () => {
                       value={query} onChange={changeHandler}
                       >
                       </input>
-                      <button><i className="fas fa-search"></i></button>
+                      <button className={styles.submit__search} type="search"><i className="fas fa-search"></i></button>
                 </form>
             </div>
         </div>
@@ -71,8 +74,13 @@ const Home = () => {
             </div>
         </div>
         <div className={styles.container}>
-        {movies.map((movieReq) =>
-        <MovieCard key={movieReq.id} {...movieReq} />
+        {movies.map((movie) =>{
+          return (
+            <MovieCard key={movie.id} {...movie}
+                        movie={movie}
+                        selectMovie={setSelectedMovie}
+            />
+          )}
         )}
         </div>
     </div>
