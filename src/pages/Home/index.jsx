7033c425b0f02a2a35a0dbabe8e7363665c8e782
Alain Noel <alain.noel34@gmail.com>
2022-12-01 19:12:59 +0100

    add responsive block

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,91 +1,98 @@
 import React, { useEffect, useState } from "react";
 import MovieCard from "../../components/MovieCard";
+import "bootstrap/dist/css/bootstrap.min.css";
 import styles from "./home.module.css";
-import SearchBar from "../../components/NavBar";
+import SearchIcon from "@mui/icons-material/Search";
 
-
-const API_URL="https://api.themoviedb.org/3/movie/popular?api_key=eda25039ca2db0c207beac572dc11346";
-const API_SEARCH="https://api.themoviedb.org/3/search/movie?api_key=eda25039ca2db0c207beac572dc11346&query";
-const IMG_PATH = "https://image.tmdb.org/t/p/500"
-
-
+const API_URL =
+  "https://api.themoviedb.org/3/movie/popular?api_key=eda25039ca2db0c207beac572dc11346";
+const API_SEARCH =
+  "https://api.themoviedb.org/3/search/movie?api_key=eda25039ca2db0c207beac572dc11346&query";
+const IMG_PATH = "https://image.tmdb.org/t/p/500";
 
 const Home = () => {
-
   const [movies, setMovies] = useState([]);
-  const [query, setQuery]=useState('');
-  const [selectMovie, setSelectedMovie]= useState([]);
+  const [query, setQuery] = useState("");
+  const [selectMovie, setSelectedMovie] = useState([]);
 
   useEffect(() => {
-     fetch(API_URL)
-    .then((res)=>res.json())
-    .then(data=>{
-      console.log(data);
-      setMovies(data.results);
-      setSelectedMovie(data.results[0]);
-      console.log(data);
-    })
-    }, [])
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setMovies(data.results);
+        setSelectedMovie(data.results[0]);
+        console.log(data);
+      });
+  }, []);
 
-    const searchMovie = async(e)=>{
-      e.preventDefault();
+  const searchMovie = async (e) => {
+    e.preventDefault();
     console.log("Searching");
-    try{
-      const url=`https://api.themoviedb.org/3/search/movie?api_key=eda25039ca2db0c207beac572dc11346&query=${query}`;
-      const res= await fetch(url);
-      const data= await res.json();
+    try {
+      const url = `https://api.themoviedb.org/3/search/movie?api_key=eda25039ca2db0c207beac572dc11346&query=${query}`;
+      const res = await fetch(url);
+      const data = await res.json();
       console.log(data);
       setMovies(data.results);
       setSelectedMovie(data.results[0]);
-    }
-    catch(e){
+    } catch (e) {
       console.log(e);
     }
-  }
+  };
 
-  const changeHandler=(e)=>{
+  const changeHandler = (e) => {
     setQuery(e.target.value);
-  }
+  };
 
   return (
     <div className="general">
-      <SearchBar />
-        <div className={styles.header}>
-            <div className={styles.header__content}>
-                <h1>AlloMovies</h1>
-                <form className="d-flex" onSubmit={searchMovie} autoComplete="on">
-                      <input type="search"
-                      placeholder="Enter Movie Name"
-                      className="inputText"
-                      arial-label="search"
-                      value={query} onChange={changeHandler}
-                      >
-                      </input>
-                      <button className={styles.submit__search} type="search"><i className="fas fa-search"></i></button>
-                </form>
-            </div>
+      <div className={styles.header}>
+        <div className={styles.header__content}>
+          <h1>AlloMovies</h1>
+          <form className="d-flex" onSubmit={searchMovie} autoComplete="on">
+            <input
+              type="search"
+              placeholder="Enter Movie Name"
+              className={styles.input__text}
+              arial-label="search"
+              value={query}
+              onChange={changeHandler}
+            />
+          </form>
         </div>
-        <div className={styles.hero} style={{backgroundImage : `url('https://image.tmdb.org/t/p/w1280${selectMovie.backdrop_path}')`}}>
+      </div>
+      <div className={styles.all__movie}>
+        <div className={styles.container}>
+          {movies.map((movie) => {
+            return (
+              <MovieCard
+                key={movie.id}
+                {...movie}
+                movie={movie}
+                selectMovie={setSelectedMovie}
+              />
+            );
+          })}
+        </div>
+        <div className={styles.poster__content}>
+          <div
+            className={styles.hero}
+            style={{
+              backgroundImage: `url('https://image.tmdb.org/t/p/w1280${selectMovie.backdrop_path}')`,
+            }}
+          >
             <div className={styles.hero__content}>
               <h1>{selectMovie.title}</h1>
               <p>Date: {selectMovie.release_date}</p>
               <p>Note: {selectMovie.vote_average}</p>
               <p>{selectMovie.overview}</p>
             </div>
+          </div>
         </div>
-        <div className={styles.container}>
-        {movies.map((movie) =>{
-          return (
-            <MovieCard key={movie.id} {...movie}
-                        movie={movie}
-                        selectMovie={setSelectedMovie}
-            />
-          )}
-        )}
-        </div>
+      </div>
     </div>
   );
-}
-
+};
 
 export default Home;
