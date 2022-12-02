f646c74878343ec0f662eaff1751dfc45c8ca908
Alain Noel <alain.noel34@gmail.com>
2022-12-02 02:33:22 +0100

    modication css

--- src/utils/tmdb.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: "https://api.themoviedb.org/3",
-  hearders: {
-      Accept: "application/json"
-  },
-  params: 'eda25039ca2db0c207beac572dc11346'
-})
